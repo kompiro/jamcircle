55010bc69471859caa1496a542e0078cf4e9ff05
Hiroki Kondo <kompiro@gmail.com>
2009-06-04 23:08:04 +0900

    added new test

---
+++ org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/boardtemplate/internal/TaskBoardTemplateTest.java
@@ -0,0 +1,28 @@
+package org.kompiro.jamcircle.kanban.boardtemplate.internal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.kompiro.jamcircle.kanban.model.Board;
+import org.kompiro.jamcircle.kanban.model.Lane;
+import org.kompiro.jamcircle.kanban.service.internal.AbstractKanbanTest;
+import org.kompiro.jamcircle.kanban.service.internal.KanbanServiceImpl;
+
+public class TaskBoardTemplateTest extends AbstractKanbanTest{
+
+	@Test
+	public void testInitialize() {
+		KanbanServiceImpl service = getKanbanService();
+		TaskBoardTemplate template = new TaskBoardTemplate();
+		Board board = service.createBoard("test");
+		template.initialize(board);
+		Lane[] lanes = board.getLanes();
+		assertNotNull(lanes);
+		assertEquals(3,lanes.length);
+		assertEquals("ToDo",lanes[0].getStatus());
+		assertEquals("Doing",lanes[1].getStatus());
+		assertEquals("Done",lanes[2].getStatus());
+		assertNotNull(lanes[2].getScript());
+	}
+
+}
