d8a4b309d67ef930b0cbda826cf057e5a8633ce1
Hiroki Kondo <kompiro@gmail.com>
2009-06-14 19:04:35 +0900

    added renamed file

---
+++ org.kompiro.jamcircle.kanban.ui.test/src/org/kompiro/jamcircle/kanban/ui/gcontroller/EditPartAllTests.java
@@ -0,0 +1,15 @@
+package org.kompiro.jamcircle.kanban.ui.gcontroller;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	BoardEditPartTest.class,
+	LaneEditPartTest.class,
+	TrashEditPartTest.class
+})
+@RunWith(Suite.class)
+public class EditPartAllTests {
+
+}
