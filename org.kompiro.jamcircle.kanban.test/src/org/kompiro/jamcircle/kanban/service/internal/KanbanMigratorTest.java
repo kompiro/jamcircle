6c8d30216c7c3bdc85e267e565476fe595050f5d
Hiroki Kondo a.k.a kompiro <kompiro@gmail.com>
2009-09-26 23:06:20 +0900

    added migrator test

---
+++ org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/service/internal/KanbanMigratorTest.java
@@ -0,0 +1,12 @@
+package org.kompiro.jamcircle.kanban.service.internal;
+
+import org.junit.Test;
+
+public class KanbanMigratorTest {
+
+	@Test
+	public void migrate() throws Exception{
+		
+	}
+	
+}
