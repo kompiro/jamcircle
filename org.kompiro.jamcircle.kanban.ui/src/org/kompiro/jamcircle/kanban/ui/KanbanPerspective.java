5d0c4f796f657ccf596396e97f4787a24c98b128
Hiroki Kondo <kompiro@gmail.com>
2009-11-04 22:31:06 +0900

    improved KanbanView Area

--- org.kompiro.jamcircle.kanban.ui/src/org/kompiro/jamcircle/kanban/ui/KanbanPerspective.java
+++ org.kompiro.jamcircle.kanban.ui/src/org/kompiro/jamcircle/kanban/ui/KanbanPerspective.java
@@ -9,19 +9,14 @@ public class KanbanPerspective implements IPerspectiveFactory {
 
 	private static final String BOARD_NEW_WIZARD = "org.kompiro.jamcircle.kanban.ui.action.BoardNewWizard";
 	
-	private static final String CONSOLE_VIEW = "org.eclipse.ui.console.ConsoleView";
 	private static final String PROGRESS_VIEW = "org.eclipse.ui.views.ProgressView";
-	private static final String KANBAN_VIEW = "org.kompiro.jamcircle.kanban.KanbanView";
 
-	private static final boolean TITLE_SHOWN = false;
 	private static final boolean EDITOR_AREA_VISIBLED = false;
 	
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(EDITOR_AREA_VISIBLED);
 		layout.addFastView(PROGRESS_VIEW);
-		layout.addStandaloneView(KANBAN_VIEW,TITLE_SHOWN, IPageLayout.TOP, 0.80f, null);
 		layout.addNewWizardShortcut(BOARD_NEW_WIZARD);
-		layout.addFastView(CONSOLE_VIEW);
 	}
 
 }
