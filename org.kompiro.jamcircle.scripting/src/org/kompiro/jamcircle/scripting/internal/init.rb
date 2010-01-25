e5dd32ac5c90d946f16391a49a8d0688127f8ad4
Hiroki Kondo(aka kompiro) <kompiro@gmail.com>
2010-01-25 10:13:33 +0900

    improved method

--- org.kompiro.jamcircle.scripting/src/org/kompiro/jamcircle/scripting/internal/init.rb
+++ org.kompiro.jamcircle.scripting/src/org/kompiro/jamcircle/scripting/internal/init.rb
@@ -7,3 +7,17 @@ lane = $bsf.lookupBean('lane');
 event = $bsf.lookupBean('event');
 board = $bsf.lookupBean('board');
 monitor = $bsf.lookupBean('monitor');
+
+def create_card(subject=nil)
+  card = Card.new
+  card.subject = subject
+  board.add_card card
+  return card
+end
+
+def create_lane(status=nil)
+  lane = Lane.new
+  lane.status = status
+  board.add_lane lane
+  return lane
+end
