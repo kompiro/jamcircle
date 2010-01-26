1f1fefea6771dda08290fa700ab5e85a3007e342
Hiroki Kondo(aka kompiro) <kompiro@gmail.com>
2010-01-26 19:40:59 +0900

    implementing...

--- org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/service/internal/KanbanBoardTemplateLoaderImplTest.java
+++ org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/service/internal/KanbanBoardTemplateLoaderImplTest.java
@@ -1,17 +1,52 @@
 package org.kompiro.jamcircle.kanban.service.internal;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertThat;
+import static org.kompiro.jamcircle.kanban.service.internal.KanbanBoardTemplateLoaderImpl.ATTR_CLASS;
+import static org.kompiro.jamcircle.kanban.service.internal.KanbanBoardTemplateLoaderImpl.POINT_CALLBACK;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.junit.Before;
 import org.junit.Test;
+import org.kompiro.jamcircle.kanban.boardtemplate.KanbanBoardTemplate;
 
 
 public class KanbanBoardTemplateLoaderImplTest {
+	
+	private KanbanBoardTemplateLoaderImpl impl;
+
+	@Before
+	public void before() throws Exception{
+		impl = new KanbanBoardTemplateLoaderImpl();
+	}
+
+	@Test
+	public void loadBoardTemplates_when_no_registered() throws Exception {
+		List<KanbanBoardTemplate> templates = impl.loadBoardTemplates();
+		assertThat(templates,not(nullValue()));
+		assertThat(templates.size(),is(0));
+	}
+	
 	@Test
 	public void loadBoardTemplates() throws Exception {
+		IExtensionRegistry registry = mock(IExtensionRegistry.class);
+		IExtension extension = mock(IExtension.class);
 		
+		when(registry.getExtension(POINT_CALLBACK)).thenReturn(extension );
+		IConfigurationElement element = mock(IConfigurationElement.class);
+		when(extension.getConfigurationElements()).thenReturn(new IConfigurationElement[]{element });
+		Object template = mock(KanbanBoardTemplate.class);
+		when(element.createExecutableExtension(ATTR_CLASS)).thenReturn(template );
 		
-		
+		impl.setRegistry(registry);
+		List<KanbanBoardTemplate> templates = impl.loadBoardTemplates();
+		assertThat(templates,not(nullValue()));
+		assertThat(templates.size(),is(1));		
 	}
 }
--- org.kompiro.jamcircle.kanban/src/org/kompiro/jamcircle/kanban/service/internal/KanbanBoardTemplateLoaderImpl.java
+++ org.kompiro.jamcircle.kanban/src/org/kompiro/jamcircle/kanban/service/internal/KanbanBoardTemplateLoaderImpl.java
@@ -1,5 +1,38 @@
 package org.kompiro.jamcircle.kanban.service.internal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.RegistryFactory;
+import org.kompiro.jamcircle.kanban.boardtemplate.KanbanBoardTemplate;
+
 public class KanbanBoardTemplateLoaderImpl {
+	
+	static final String POINT_CALLBACK = "org.kompiro.jamcircle.kanban.boardTemplate";
+	static final String ATTR_CLASS = "class";
+	private IExtensionRegistry registry = RegistryFactory.getRegistry();
+
+	public List<KanbanBoardTemplate> loadBoardTemplates() {
+		ArrayList<KanbanBoardTemplate> result = new ArrayList<KanbanBoardTemplate>();
+		IExtension extension = registry.getExtension(POINT_CALLBACK);
+		IConfigurationElement[] configurationElements = extension.getConfigurationElements();
+		if(configurationElements == null) return result;
+		for (IConfigurationElement configurationElement : configurationElements) {
+			try {
+				KanbanBoardTemplate template = (KanbanBoardTemplate) configurationElement.createExecutableExtension(ATTR_CLASS);
+				result.add(template);
+			} catch (CoreException e) {
+			}
+		}
+		return result;
+	}
+	
+	public void setRegistry(IExtensionRegistry registry) {
+		this.registry = registry;
+	}
 
 }
