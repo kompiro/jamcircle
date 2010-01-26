8bd1d2e123c67efb61aa46579b76fb1e859887e0
Hiroki Kondo(aka kompiro) <kompiro@gmail.com>
2010-01-26 17:38:20 +0900

    added test for kanban board template loader

---
+++ org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/service/internal/KanbanBoardTemplateLoaderImplTest.java
@@ -0,0 +1,17 @@
+package org.kompiro.jamcircle.kanban.service.internal;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+
+public class KanbanBoardTemplateLoaderImplTest {
+	@Test
+	public void loadBoardTemplates() throws Exception {
+		
+		
+		
+	}
+}
