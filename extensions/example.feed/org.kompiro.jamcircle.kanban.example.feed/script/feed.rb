8f318ada62c15f4948608449a3d480543b6621f1
Hiroki Kondo <kompiro@gmail.com>
2010-05-06 11:06:53 +0900

    improved feed extension

--- extensions/example.feed/org.kompiro.jamcircle.kanban.example.feed/script/feed.rb
+++ extensions/example.feed/org.kompiro.jamcircle.kanban.example.feed/script/feed.rb
@@ -1,1 +1,11 @@
-create_card 'feed test'
+require 'open-uri'
+require 'rexml/document'
+
+feed_url = "http://rss.news.yahoo.com/rss/topstories"
+
+xml = open(feed_url).read
+doc = REXML::Document.new(xml)
+doc.each_element('/rss/channel/item') do |elem|
+  card = create_card  elem.elements['title'].text
+  card.content = elem.elements['description'].text
+end
