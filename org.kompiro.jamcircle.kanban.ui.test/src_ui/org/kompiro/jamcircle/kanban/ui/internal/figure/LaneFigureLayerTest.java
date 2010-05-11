c6d31054232b0f87a38f111d842524fd18bde549
Hiroki Kondo <kompiro@gmail.com>
2010-05-11 22:49:53 +0900

    fixed test

--- org.kompiro.jamcircle.kanban.ui.test/src_ui/org/kompiro/jamcircle/kanban/ui/internal/figure/LaneFigureLayerTest.java
+++ org.kompiro.jamcircle.kanban.ui.test/src_ui/org/kompiro/jamcircle/kanban/ui/internal/figure/LaneFigureLayerTest.java
@@ -1,6 +1,7 @@
 package org.kompiro.jamcircle.kanban.ui.internal.figure;
 
 import org.eclipse.draw2d.LightweightSystem;
+import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -17,19 +18,16 @@ public class LaneFigureLayerTest  {
 	
 	@Test
 	public void createFigure() throws Exception {
+		
 		Display display = Display.getDefault();
 		Shell shell = new Shell(display);
 		shell.setLayout(new FillLayout());
 		LightweightSystem lws = new LightweightSystem(shell);
 		LaneFigureLayer figure = new LaneFigureLayer();
+		figure.setSize(200,500);
 		figure.setStatus("test");
 		lws.setContents(figure);
-		shell.pack();
 		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) display.sleep();
-		}
-		display.dispose();
 	}
 
 }
