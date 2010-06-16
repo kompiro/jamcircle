4274540ad376e77cf641f3d87f59ee1338e5b6d1
Hiroki Kondo <kompiro@gmail.com>
2010-06-17 01:03:19 +0900

    fixed can't get display when workbench is starting.

--- org.kompiro.jamcircle.storage.ui/src/org/kompiro/jamcircle/storage/ui/StorageCallbackHandler.java
+++ org.kompiro.jamcircle.storage.ui/src/org/kompiro/jamcircle/storage/ui/StorageCallbackHandler.java
@@ -1,8 +1,11 @@
 package org.kompiro.jamcircle.storage.ui;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.kompiro.jamcircle.storage.IUIStorageCallbackHandler;
 import org.kompiro.jamcircle.storage.ui.wizard.StorageSettingWizard;
@@ -14,7 +17,7 @@ public class StorageCallbackHandler implements IUIStorageCallbackHandler {
 	}
 
 	public void setupStorageSttting() {
-		Display display = PlatformUI.getWorkbench().getDisplay();
+		Display display = getDisplay();
 		display.syncExec(new Runnable(){
 
 			public void run() {
@@ -25,4 +28,32 @@ public class StorageCallbackHandler implements IUIStorageCallbackHandler {
 		});
 	}
 
+	public Display getDisplay(){
+		IWorkbench workbench = getWorkbench();
+		Display defDisplay = Display.getDefault();
+		if(workbench == null){
+			return defDisplay;
+		}
+		try{
+			if(workbench == null) return defDisplay;
+			Display display = workbench.getDisplay();
+			if(display != null) return display;
+			IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
+			if(activeWorkbenchWindow == null) return defDisplay;
+			display = activeWorkbenchWindow.getShell().getDisplay();
+			return display;
+		}catch(IllegalStateException e){
+			return defDisplay;
+		}
+	}
+	
+	public IWorkbench getWorkbench() {
+		if(Platform.isRunning()){
+			if(PlatformUI.isWorkbenchRunning()){
+				return PlatformUI.getWorkbench();
+			}
+		}
+		return null;
+	}
+	
 }
