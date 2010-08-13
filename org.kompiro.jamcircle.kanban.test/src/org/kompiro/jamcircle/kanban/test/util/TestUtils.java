fad9f145d0612f6195df1d20ee1f05a47126be73
Hiroki Kondo <kompiro@gmail.com>
2010-08-13 19:45:19 +0900

    added getFile method

--- org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/test/util/TestUtils.java
+++ org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/test/util/TestUtils.java
@@ -1,16 +1,33 @@
 package org.kompiro.jamcircle.kanban.test.util;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import java.io.*;
 import java.net.URL;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.kompiro.jamcircle.kanban.model.Board;
+import org.kompiro.jamcircle.kanban.model.Lane;
 
 public class TestUtils {
 	public static final String LONG_TXT_FILE = "long.txt";
 	public static final String LONG_TXT = "/org/kompiro/jamcircle/kanban/test/util/"
 			+ LONG_TXT_FILE;
+
+	public File getFile(Class<?> clazz, String path) throws Exception {
+		URL url = clazz.getResource(path);
+		if (!url.getProtocol().equals("file")) {
+			url = FileLocator.toFileURL(url);
+		}
+		if ("file".equals(url.getProtocol())) {
+			path = url.getPath().replaceAll("%20", " ");
+			return new File(path);
+		}
+		return null;
+	}
 
 	public String readFile(String path) throws Exception {
 		return readFile(getClass(), path);
@@ -70,6 +87,70 @@ public class TestUtils {
 			}
 			file.delete();
 		}
+	}
+
+	public void setMockIcon(Lane target, String fileName) throws IOException {
+		File base = File.createTempFile("image", ".png");
+		File image = spy(base);
+		when(image.getName()).thenReturn(fileName);
+		when(target.getCustomIcon()).thenReturn(image);
+	}
+
+	public Lane createMockLane(final int id, String title, int x, int y, int width, int height) {
+		return createMockLane(id, title, x, y, width, height, false);
+	}
+
+	public Lane createMockLane(final int id, String title, int x, int y, int width, int height, boolean iconized) {
+		org.kompiro.jamcircle.kanban.model.mock.Lane lane = new org.kompiro.jamcircle.kanban.model.mock.Lane() {
+			@Override
+			public int getID() {
+				return id;
+			}
+		};
+		lane.setStatus(title);
+		lane.setX(x);
+		lane.setY(y);
+		lane.setWidth(width);
+		lane.setHeight(height);
+		lane.setIconized(iconized);
+		return lane;
+	}
+
+	public Board createMockBoard(String title) {
+		org.kompiro.jamcircle.kanban.model.mock.Board board = new org.kompiro.jamcircle.kanban.model.mock.Board();
+		board.setTitle(title);
+		return board;
+	}
+
+	public Board createBoard(String title) {
+		return createBoard(title, null);
+	}
+
+	public Board createBoard(String title, Lane[] lanes) {
+		Board board = mock(Board.class);
+		when(board.getTitle()).thenReturn(title);
+		if (lanes != null) {
+			when(board.getLanes()).thenReturn(lanes);
+		}
+		return board;
+	}
+
+	public Lane createLane(int id, String status, int locationX,
+			int locationY, int width, int height) {
+		return createLane(id, status, locationX, locationY, width, height, false);
+	}
+
+	public Lane createLane(int id, String status, int locationX,
+			int locationY, int width, int height, boolean iconized) {
+		Lane lane = mock(Lane.class);
+		when(lane.getID()).thenReturn(id);
+		when(lane.getStatus()).thenReturn(status);
+		when(lane.getX()).thenReturn(locationX);
+		when(lane.getY()).thenReturn(locationY);
+		when(lane.getWidth()).thenReturn(width);
+		when(lane.getHeight()).thenReturn(height);
+		when(lane.isIconized()).thenReturn(iconized);
+		return lane;
 	}
 
 }
--- org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/test/util/TestUtilsTest.java
+++ org.kompiro.jamcircle.kanban.test/src/org/kompiro/jamcircle/kanban/test/util/TestUtilsTest.java
@@ -1,7 +1,11 @@
 package org.kompiro.jamcircle.kanban.test.util;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -9,43 +13,50 @@ import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import org.kompiro.jamcircle.kanban.service.internal.BoardConverterImplTest;
 
 public class TestUtilsTest {
 	private TestUtils testUtils;
 
 	@Test
-	public void testReadFile_absolute_path() throws Exception {
+	public void getFile() throws Exception {
+		File file = testUtils.getFile(BoardConverterImplTest.class, "Sample Board.zip");
+		assertThat(file, not(nullValue()));
+		System.out.println(file.getAbsolutePath());
+		assertThat(file.exists(), is(true));
+	}
+
+	@Test
+	public void readFile_absolute_path() throws Exception {
 		String stringFromFile = testUtils.readFile(TestUtils.LONG_TXT_FILE);
 		assertNotNull(stringFromFile);
 		assertNotSame(0, stringFromFile.length());
 	}
 
 	@Test
-	public void testReadFile_canonical_path() throws Exception {
+	public void readFile_canonical_path() throws Exception {
 		String stringFromFile = testUtils.readFile(TestUtils.LONG_TXT);
 		assertNotNull(stringFromFile);
 		assertNotSame(0, stringFromFile.length());
 	}
 
 	@Test
-	public void testReadFile_canonical_path_and_specify_class() throws Exception {
-		String stringFromFile = testUtils.readFile(getClass(),TestUtils.LONG_TXT);
+	public void readFile_canonical_path_and_specify_class() throws Exception {
+		String stringFromFile = testUtils.readFile(getClass(), TestUtils.LONG_TXT);
 		assertNotNull(stringFromFile);
 		assertNotSame(0, stringFromFile.length());
 	}
 
-	
 	@Test
-	public void testTarget() throws Exception {
+	public void target() throws Exception {
 		File target = testUtils.target();
 		assertNotNull(target);
 	}
-	
+
 	@Test
 	public void travasalDelteWhenFileListIsNull() throws Exception {
 		File file = mock(File.class);
-		
+
 		when(file.listFiles()).thenReturn(null);
 		testUtils.travasalDelete(file);
 	}
