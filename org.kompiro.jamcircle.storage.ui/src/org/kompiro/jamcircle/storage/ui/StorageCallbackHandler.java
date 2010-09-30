6287095516a40d655e8bcf09705b554a52043078
Hiroki Kondo <kompiro@gmail.com>
2010-10-01 02:23:56 +0900

    fixed warning

--- org.kompiro.jamcircle.storage.ui/src/org/kompiro/jamcircle/storage/ui/StorageCallbackHandler.java
+++ org.kompiro.jamcircle.storage.ui/src/org/kompiro/jamcircle/storage/ui/StorageCallbackHandler.java
@@ -4,12 +4,9 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.*;
 import org.kompiro.jamcircle.storage.IUIStorageCallbackHandler;
 import org.kompiro.jamcircle.storage.ui.wizard.StorageSettingWizard;
-
 
 public class StorageCallbackHandler implements IUIStorageCallbackHandler {
 
@@ -18,7 +15,7 @@ public class StorageCallbackHandler implements IUIStorageCallbackHandler {
 
 	public void setupStorageSttting() {
 		Display display = getDisplay();
-		display.syncExec(new Runnable(){
+		display.syncExec(new Runnable() {
 
 			public void run() {
 				StorageSettingWizard wizard = new StorageSettingWizard();
@@ -28,32 +25,31 @@ public class StorageCallbackHandler implements IUIStorageCallbackHandler {
 		});
 	}
 
-	public Display getDisplay(){
+	public Display getDisplay() {
 		IWorkbench workbench = getWorkbench();
-		Display defDisplay = Display.getDefault();
-		if(workbench == null){
-			return defDisplay;
-		}
-		try{
-			if(workbench == null) return defDisplay;
+		try {
+			if (workbench == null)
+				return null;
 			Display display = workbench.getDisplay();
-			if(display != null) return display;
+			if (display != null)
+				return display;
 			IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
-			if(activeWorkbenchWindow == null) return defDisplay;
+			if (activeWorkbenchWindow == null)
+				return null;
 			display = activeWorkbenchWindow.getShell().getDisplay();
 			return display;
-		}catch(IllegalStateException e){
-			return defDisplay;
+		} catch (IllegalStateException e) {
+			return null;
 		}
 	}
-	
+
 	public IWorkbench getWorkbench() {
-		if(Platform.isRunning()){
-			if(PlatformUI.isWorkbenchRunning()){
+		if (Platform.isRunning()) {
+			if (PlatformUI.isWorkbenchRunning()) {
 				return PlatformUI.getWorkbench();
 			}
 		}
 		return null;
 	}
-	
+
 }
