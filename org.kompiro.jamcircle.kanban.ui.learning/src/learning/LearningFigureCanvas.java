f58f2960ea18cf070d891eeb9d4f57c3aa0bff2b
Hiroki Kondo <kompiro@gmail.com>
2010-10-23 00:57:46 +0900

    moved kanban ui leaning to new project

--- org.kompiro.jamcircle.kanban.ui.learning/src/learning/LearningFigureCanvas.java
+++ org.kompiro.jamcircle.kanban.ui.learning/src/learning/LearningFigureCanvas.java
@@ -4,18 +4,13 @@ import org.eclipse.draw2d.FigureCanvas;
 import org.eclipse.draw2d.LightweightSystem;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.snippets.Snippet237;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.*;
 
 public class LearningFigureCanvas {
-	
+
 	public static void main(String[] args) {
 		final Display display = new Display();
 		final Shell shell = new Shell(display);
@@ -26,19 +21,20 @@ public class LearningFigureCanvas {
 		org.eclipse.draw2d.Label figure = new org.eclipse.draw2d.Label();
 		figure.setText("test");
 		lws.getRootFigure().add(figure);
-		FigureCanvas canvas = new FigureCanvas(SWT.H_SCROLL|SWT.V_SCROLL|SWT.NO_REDRAW_RESIZE,composite , lws);
-		GridDataFactory.fillDefaults().hint(200,200).applyTo(canvas);
+		FigureCanvas canvas = new FigureCanvas(SWT.H_SCROLL | SWT.V_SCROLL | SWT.NO_REDRAW_RESIZE, composite, lws);
+		GridDataFactory.fillDefaults().hint(200, 200).applyTo(canvas);
 		ImageLoader loader = new ImageLoader();
 		ImageData[] load = loader.load(new Snippet237().getClass().getResourceAsStream("trac_bullet.png"));
-		Image image = new Image(Display.getDefault(),load[0]);
+		Image image = new Image(Display.getDefault(), load[0]);
 		canvas.setBackgroundImage(image);
 		canvas.setBackground(color);
 		canvas.pack();
 
 		shell.pack();
 		shell.open();
-		while(!shell.isDisposed()) {
-			if(!display.readAndDispatch()) display.sleep();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
 		}
 		display.dispose();
 
