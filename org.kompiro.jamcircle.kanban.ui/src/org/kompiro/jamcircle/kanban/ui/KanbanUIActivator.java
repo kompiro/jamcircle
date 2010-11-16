a764f69f9caed4a5f8f53cb8f2e85eaad19c06f1
Hiroki Kondo <kompiro@gmail.com>
2010-11-16 23:47:14 +0900

    fixed handler calling

--- org.kompiro.jamcircle.kanban.ui/src/org/kompiro/jamcircle/kanban/ui/KanbanUIActivator.java
+++ org.kompiro.jamcircle.kanban.ui/src/org/kompiro/jamcircle/kanban/ui/KanbanUIActivator.java
@@ -1,6 +1,7 @@
 package org.kompiro.jamcircle.kanban.ui;
 
 import static java.lang.String.format;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -17,19 +18,25 @@ import org.kompiro.jamcircle.storage.StorageStatusHandler;
 import org.osgi.framework.BundleContext;
 
 public class KanbanUIActivator extends AbstractUIPlugin {
-	
+
 	private final class MessageDialogStatusHandler implements IStatusHandler {
 		public void displayStatus(String title, IStatus status) {
 		}
 
-		public void fail(IStatus status, boolean informUser) {
-			Throwable exception = status.getException();
-			if(informUser && ! getKanbanService().isTestMode()){
-				String message = format(Messages.KanbanUIActivator_error_message, status.getMessage(),exception.getClass().getName(),exception.getLocalizedMessage());
-				Shell shell = getShell();
-				if(shell != null){
-					MessageDialog.openError(shell, Messages.KanbanUIActivator_error_title,message);
-				}
+		public void fail(final IStatus status, boolean informUser) {
+			final Throwable exception = status.getException();
+			if (informUser && !getKanbanService().isTestMode()) {
+				WorkbenchUtil.async(new Runnable() {
+					public void run() {
+						String message = format(Messages.KanbanUIActivator_error_message, status.getMessage(),
+								exception
+										.getClass().getName(), exception.getLocalizedMessage());
+						Shell shell = getShell();
+						if (shell != null) {
+							MessageDialog.openError(shell, Messages.KanbanUIActivator_error_title, message);
+						}
+					}
+				});
 			}
 		}
 
@@ -45,22 +52,20 @@ public class KanbanUIActivator extends AbstractUIPlugin {
 
 	public KanbanUIActivator() {
 	}
-	
 
 	public void start(BundleContext context) throws Exception {
 		plugin = this;
 		super.start(context);
 
 		handleDialogForStatusHandler();
-//		migrate();
+		// migrate();
 	}
-
 
 	private void handleDialogForStatusHandler() {
 		dialogHandler = new MessageDialogStatusHandler();
- 		StorageStatusHandler.addStatusHandler(dialogHandler);
- 		KanbanStatusHandler.addStatusHandler(dialogHandler);
- 		KanbanUIStatusHandler.addStatusHandler(dialogHandler);
+		StorageStatusHandler.addStatusHandler(dialogHandler);
+		KanbanStatusHandler.addStatusHandler(dialogHandler);
+		KanbanUIStatusHandler.addStatusHandler(dialogHandler);
 	}
 
 	public void stop(BundleContext context) throws Exception {
@@ -71,46 +76,45 @@ public class KanbanUIActivator extends AbstractUIPlugin {
 		StorageStatusHandler.removeStatusHandler(dialogHandler);
 		super.stop(context);
 	}
-	
+
 	@Override
 	protected void initializeImageRegistry(ImageRegistry reg) {
-		initializeImage(reg,KanbanImageConstants.OPEN_LIST_ACTION_IMAGE);
-		initializeImage(reg,KanbanImageConstants.TRASH_FULL_IMAGE);
-		initializeImage(reg,KanbanImageConstants.TRASH_EMPTY_IMAGE);
-		initializeImage(reg,KanbanImageConstants.SEND_ON_IMAGE);
-		initializeImage(reg,KanbanImageConstants.SEND_OFF_IMAGE);
-		initializeImage(reg,KanbanImageConstants.CONNECT_IMAGE);
-		initializeImage(reg,KanbanImageConstants.DISCONNECT_IMAGE);
-		initializeImage(reg,KanbanImageConstants.BACKGROUND_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FILE_LINK_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FILE_GO_IMAGE);
-		initializeImage(reg,KanbanImageConstants.LANE_ICONIZE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.LANE_RESTORE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.USER_IMAGE);
-		initializeImage(reg,KanbanImageConstants.COLOR_IMAGE);
-		initializeImage(reg,KanbanImageConstants.ADD_IMAGE);
-		initializeImage(reg,KanbanImageConstants.EDIT_IMAGE);
-		initializeImage(reg,KanbanImageConstants.KANBANS_IMAGE);
-		initializeImage(reg,KanbanImageConstants.PAGE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.COMPLETED_IMAGE);
-		initializeImage(reg,KanbanImageConstants.INBOX_IMAGE);
-		initializeImage(reg,KanbanImageConstants.CAMERA_IMAGE);
-		initializeImage(reg,KanbanImageConstants.SAVE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.CLOCK_IMAGE);
-		initializeImage(reg,KanbanImageConstants.CLOCK_RED_IMAGE);
-		initializeImage(reg,KanbanImageConstants.DELETE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.OPEN_IMAGE);
-		initializeImage(reg,KanbanImageConstants.MOCK_IMAGE);
-		initializeImage(reg,KanbanImageConstants.DB_ADD_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FLAG_BLUE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FLAG_GREEN_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FLAG_ORANGE_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FLAG_RED_IMAGE);
-		initializeImage(reg,KanbanImageConstants.FLAG_WHITE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.OPEN_LIST_ACTION_IMAGE);
+		initializeImage(reg, KanbanImageConstants.TRASH_FULL_IMAGE);
+		initializeImage(reg, KanbanImageConstants.TRASH_EMPTY_IMAGE);
+		initializeImage(reg, KanbanImageConstants.SEND_ON_IMAGE);
+		initializeImage(reg, KanbanImageConstants.SEND_OFF_IMAGE);
+		initializeImage(reg, KanbanImageConstants.CONNECT_IMAGE);
+		initializeImage(reg, KanbanImageConstants.DISCONNECT_IMAGE);
+		initializeImage(reg, KanbanImageConstants.BACKGROUND_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FILE_LINK_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FILE_GO_IMAGE);
+		initializeImage(reg, KanbanImageConstants.LANE_ICONIZE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.LANE_RESTORE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.USER_IMAGE);
+		initializeImage(reg, KanbanImageConstants.COLOR_IMAGE);
+		initializeImage(reg, KanbanImageConstants.ADD_IMAGE);
+		initializeImage(reg, KanbanImageConstants.EDIT_IMAGE);
+		initializeImage(reg, KanbanImageConstants.KANBANS_IMAGE);
+		initializeImage(reg, KanbanImageConstants.PAGE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.COMPLETED_IMAGE);
+		initializeImage(reg, KanbanImageConstants.INBOX_IMAGE);
+		initializeImage(reg, KanbanImageConstants.CAMERA_IMAGE);
+		initializeImage(reg, KanbanImageConstants.SAVE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.CLOCK_IMAGE);
+		initializeImage(reg, KanbanImageConstants.CLOCK_RED_IMAGE);
+		initializeImage(reg, KanbanImageConstants.DELETE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.OPEN_IMAGE);
+		initializeImage(reg, KanbanImageConstants.MOCK_IMAGE);
+		initializeImage(reg, KanbanImageConstants.DB_ADD_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FLAG_BLUE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FLAG_GREEN_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FLAG_ORANGE_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FLAG_RED_IMAGE);
+		initializeImage(reg, KanbanImageConstants.FLAG_WHITE_IMAGE);
 	}
 
-
-	private void initializeImage(ImageRegistry reg,KanbanImageConstants constants) {
+	private void initializeImage(ImageRegistry reg, KanbanImageConstants constants) {
 		String PLUGIN_ID = getBundle().getSymbolicName();
 		reg.put(constants.toString(), imageDescriptorFromPlugin(PLUGIN_ID, constants.getPath()));
 	}
@@ -118,29 +122,31 @@ public class KanbanUIActivator extends AbstractUIPlugin {
 	public static KanbanUIActivator getDefault() {
 		return plugin;
 	}
-	
-	public KanbanService getKanbanService(){
+
+	public KanbanService getKanbanService() {
 		KanbanUIContext context = KanbanUIContext.getDefault();
-		if(context == null) throw new IllegalStateException(Messages.KanbanUIActivator_error_kanban_context);		
+		if (context == null)
+			throw new IllegalStateException(Messages.KanbanUIActivator_error_kanban_context);
 		KanbanService service = context.getKanbanService();
-		if(service == null) throw new IllegalStateException(Messages.KanbanUIActivator_error_kanban_service);		
-		service .init();
+		if (service == null)
+			throw new IllegalStateException(Messages.KanbanUIActivator_error_kanban_service);
+		service.init();
 		return service;
 	}
 
-	public Shell getShell(){
+	public Shell getShell() {
 		return WorkbenchUtil.getShell();
 	}
 
-
 	public ScriptingService getScriptingService() throws ScriptingException {
 		ScriptingService service = KanbanUIContext.getDefault().getScriptingService();
-		if(service == null) throw new IllegalStateException(Messages.KanbanUIActivator_error_scripting_service);		
+		if (service == null)
+			throw new IllegalStateException(Messages.KanbanUIActivator_error_scripting_service);
 		return service;
 	}
-	
-	public static IStatus createErrorStatus(Throwable e){
-		return new Status(IStatus.ERROR, ID_PLUGIN, e.getLocalizedMessage(),e);
+
+	public static IStatus createErrorStatus(Throwable e) {
+		return new Status(IStatus.ERROR, ID_PLUGIN, e.getLocalizedMessage(), e);
 	}
 
 }
