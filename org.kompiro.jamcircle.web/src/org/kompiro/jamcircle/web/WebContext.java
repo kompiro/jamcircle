72e6d531be75172b885306c7a91eee2348bfb691
Hiroki Kondo <kompiro@gmail.com>
2010-11-29 08:07:22 +0900

    integrated with web and kanban

---
+++ org.kompiro.jamcircle.web/src/org/kompiro/jamcircle/web/WebContext.java
@@ -0,0 +1,49 @@
+package org.kompiro.jamcircle.web;
+
+import org.kompiro.jamcircle.kanban.service.BoardConverter;
+import org.kompiro.jamcircle.kanban.service.KanbanService;
+import org.kompiro.jamcircle.scripting.ScriptingService;
+
+public class WebContext {
+
+	private static WebContext context;
+	private KanbanService kanbanService;
+	private BoardConverter boardConverter;
+	private ScriptingService scriptingService;
+
+	public WebContext() {
+		WebContext.context = this;
+	}
+
+	public static WebContext getDefault() {
+		if (context == null) {
+			WebContext.context = new WebContext();
+		}
+		return context;
+	}
+
+	public KanbanService getKanbanService() {
+		return kanbanService;
+	}
+
+	public void setKanbanService(KanbanService kanbanService) {
+		this.kanbanService = kanbanService;
+	}
+
+	public ScriptingService getScriptingService() {
+		return scriptingService;
+	}
+
+	public void setScriptingService(ScriptingService scriptingService) {
+		this.scriptingService = scriptingService;
+	}
+
+	public void setBoardConverter(BoardConverter boardConverter) {
+		this.boardConverter = boardConverter;
+	}
+
+	public BoardConverter getBoardConverter() {
+		return boardConverter;
+	}
+
+}
