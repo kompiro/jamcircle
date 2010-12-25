4dc8820abf9fb2f2247f89ac4892ca8294e43e4e
Hiroki Kondo <kompiro@gmail.com>
2010-12-25 16:18:35 +0900

    removed KanbanScriptingEngineInitializerTest

--- org.kompiro.jamcircle.scripting.kanban.test/src/org/kompiro/jamcircle/scripting/kanban/internal/KanbanScriptingEngineInitializerTest.java
+++ org.kompiro.jamcircle.scripting.kanban.test/src/org/kompiro/jamcircle/scripting/kanban/internal/KanbanScriptingEngineInitializerTest.java
@@ -1,24 +1,32 @@
 package org.kompiro.jamcircle.scripting.kanban.internal;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+import org.kompiro.jamcircle.kanban.model.ColorTypes;
+import org.kompiro.jamcircle.kanban.model.FlagTypes;
 
 public class KanbanScriptingEngineInitializerTest {
 
 	@Test
-	public void init() throws Exception {
+	public void initializeColorTypes() throws Exception {
 		KanbanScriptingEngineInitializer initializer = new KanbanScriptingEngineInitializer();
-		HashMap<String, Object> beans = new HashMap<String, Object>();
+		Map<String, Object> beans = new HashMap<String, Object>();
 		initializer.init(beans);
-		assertThat(beans.size(), is(not(0)));
-		assertThat(beans.get("RED"), is(notNullValue()));
-		assertThat(beans.get("FLAG_RED"), is(notNullValue()));
+
+		assertEquals(beans.get("RED"), ColorTypes.RED);
+	}
+
+	@Test
+	public void initializeFlagTypes() throws Exception {
+		KanbanScriptingEngineInitializer initializer = new KanbanScriptingEngineInitializer();
+		Map<String, Object> beans = new HashMap<String, Object>();
+		initializer.init(beans);
+
+		assertEquals(beans.get("FLAG_RED"), FlagTypes.RED);
 	}
 
 }
