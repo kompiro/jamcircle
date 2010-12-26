c509dfbfe4f2d2b95d51431a7df92282f2ba2966
Hiroki Kondo <kompiro@gmail.com>
2010-12-26 19:11:10 +0900

    enabled to lock when saving

--- org.kompiro.jamcircle.storage/src/org/kompiro/jamcircle/storage/model/DefaultExecutorHandle.java
+++ org.kompiro.jamcircle.storage/src/org/kompiro/jamcircle/storage/model/DefaultExecutorHandle.java
@@ -1,11 +1,11 @@
 package org.kompiro.jamcircle.storage.model;
 
 import org.eclipse.core.runtime.*;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.*;
 import org.kompiro.jamcircle.storage.service.internal.StorageAccessRule;
 
 public class DefaultExecutorHandle implements ExecutorHandler {
+	private static ILock lock = Job.getJobManager().newLock();
 
 	public void handle(final Runnable runtime) {
 		Job job = new Job("save entity") {
@@ -18,6 +18,11 @@ public class DefaultExecutorHandle implements ExecutorHandler {
 		ISchedulingRule rule = new StorageAccessRule();
 		job.setRule(rule);
 		job.setSystem(true);
-		job.schedule();
+		try {
+			lock.acquire();
+			job.schedule();
+		} finally {
+			lock.release();
+		}
 	}
 }
